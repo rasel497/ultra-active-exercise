d6130a0549a26c7eddcb90f28e847c4fb37963eb
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-29 14:20:10 +0600

    profile and add break UI done

--- src/componants/ExerciseProfile/ExerciseProfile.js
+++ src/componants/ExerciseProfile/ExerciseProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ExerciseProfile.css'
 
+
 const ExerciseProfile = () => {
 
 
@@ -10,9 +11,34 @@ const ExerciseProfile = () => {
 
     return (
         <div className='cart'>
-            <h4>My Profile</h4>
+            <div className="my-profile">
+                <img src="" alt="" />
 
+                <div className="my-name">
+                    <nav>
+                        <a className="name" href="/name">Rashel Hossain</a> <br />
+                        <a href="/address">Dhaka, Bangladsh</a>
+                    </nav>
+                </div>
 
+                <div className="mydetails">
+                    <div className="detail-card">
+                        <a href="0">59kg</a>
+                        <a href="0">5.6</a>
+                        <a href="0">25yrs</a>
+                    </div>
+                </div>
+
+                <div className="add-break">
+                    <h4>Add A Break</h4>
+                    <div className="break-card">
+                        <a href="0">10s</a>
+                        <a href="0">20s</a>
+                        <a href="0">30s</a>
+                        <a href="0">40s</a>
+                    </div>
+                </div>
+            </div>
             <button onClick={activityCompleted} className="acivity-btn">Activity Completed</button>
         </div>
 
